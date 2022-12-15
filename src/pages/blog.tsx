e11467e53742c9fe50e4821c6dfaf6d6d1a0b4e2
Pebi Sandi <pebisandi04@gmail.com>
2022-12-15 17:43:01 +0700

    add: init starter, configuration serversideprops

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,9 +1,29 @@
 import { AdminLayout } from "@layout";
+import axios from "axios";
 
-const Blog = () => {
+export type Props = {
+  data: ApiResult;
+}
+
+export type ApiResult = {
+  title?: string;
+  userId?: number;
+}
+
+export async function getServerSideProps() {
+  // Fetch data from external API
+  const res = await axios(`https://jsonplaceholder.typicode.com/todos/1`)
+  const data = res.data;
+
+  // Pass data to the page via props
+  return { props: { data } }
+}
+
+const Blog = ({data}:Props) => {
   return (
     <AdminLayout>
-      <div>Blog</div>
+      <div>{data.userId}</div>
+      <h1>{data.title}</h1>
     </AdminLayout>
  );
 }
